e4177b422dc3b1d17f0a39f7754a8732092912c5
Brian Davis <bd@jollyrogerlabs.com>
2016-09-03 13:49:16 -0400

    Added test cases for variadic template policy matching.

--- test/test_jrl_metaprogram.cpp
+++ test/test_jrl_metaprogram.cpp
@@ -34,7 +34,6 @@
  *
  */
 
-#include <iostream>
 #include <vector>
 #include <type_traits>
 #include <typeinfo>
@@ -79,10 +78,23 @@ TEST_CASE("SELECTIVE_CONST", "[SELECTIVE_CONST]")
 }
 
 
-// test cases for variadic policy support
+// test cases for variadic template parameter pack counting
 
 template<typename... ParameterPack>
-struct variadic_test
+struct variadic_test0
+{
+  static unsigned
+  count()
+  {
+    return counter_type::value;
+  }
+
+private:
+  using counter_type = param_pack_size<ParameterPack...>;
+};
+
+template<typename Dummy, typename... ParameterPack>
+struct variadic_test1
 {
   static unsigned
   count()
@@ -97,17 +109,157 @@ private:
 TEST_CASE("Test parameter pack counting", "[param_pack_counter]")
 {
   {
-    using Count0 = variadic_test<>;
+    using Count0 = variadic_test0<>;
     REQUIRE(0 == Count0::count());
   }
 
   {
-    using Count1 = variadic_test<int>;
+    using Count1 = variadic_test0<int>;
     REQUIRE(1 == Count1::count());
   }
 
   {
-    using Count2 = variadic_test<int, double>;
+    using Count2 = variadic_test0<int, double>;
+    REQUIRE(2 == Count2::count());
+  }
+  {
+    using Count0 = variadic_test1<int>;
+    REQUIRE(0 == Count0::count());
+  }
+
+  {
+    using Count1 = variadic_test1<int, int>;
+    REQUIRE(1 == Count1::count());
+  }
+
+  {
+    using Count2 = variadic_test1<int, int, double>;
     REQUIRE(2 == Count2::count());
   }
 }
+
+
+// test cases for variadic template policy matching
+
+struct name_policy_tag {};
+
+struct default_name_policy
+{
+  template<typename StringType>
+  struct rebind
+  {
+    static constexpr StringType
+    name()
+    {
+      return "Foo";
+    }
+  };
+};
+
+struct bar_name_policy : private name_policy_tag
+{
+  template<typename StringType>
+  struct rebind
+  {
+    static constexpr StringType
+    name()
+    {
+      return "Bar";
+    }
+  };
+};
+
+DEFINE_POLICY_BINDER(name_policy_binder,
+		     default_name_policy,
+		     name_policy_tag);
+
+template<typename StringType, typename... Policies>
+struct NamePolicyTest
+{
+  using NamePolicyBinding = name_policy_binder<Policies...>;
+  using NamePolicy = typename NamePolicyBinding::type;
+  using ParameterCount = param_pack_size<Policies...>;
+  using PolicyCount = policy_bind_counter<NamePolicyBinding>;
+
+  static_assert((ParameterCount::value == PolicyCount::value),
+  		"extraneous type specified in NamePolicyTest policies");
+
+  using Name =
+    typename NamePolicy::template rebind<StringType>;
+
+  static constexpr StringType
+  name()
+  {
+    return Name::name();
+  }
+
+  static constexpr unsigned
+  parameters()
+  {
+    return ParameterCount::value;
+  }
+
+  static constexpr unsigned
+  policies()
+  {
+    return PolicyCount::value;
+  }
+};
+
+namespace
+{
+const string FOO_NAME_STRING = "Foo";
+const string BAR_NAME_STRING = "Bar";
+const char *FOO_NAME_CSTRING = "Foo";
+const char *BAR_NAME_CSTRING = "Bar";
+}
+
+TEST_CASE("policy binding test", "[binding]")
+{
+  using StringFooName = NamePolicyTest<string>;
+  using CStringFooName = NamePolicyTest<const char *>;
+  using StringBarName = NamePolicyTest<string, bar_name_policy>;
+  using CStringBarName = NamePolicyTest<const char *, bar_name_policy>;
+
+  REQUIRE(StringFooName::parameters() == 0);
+  REQUIRE(CStringFooName::parameters() == 0);
+  REQUIRE(StringBarName::parameters() == 1);
+  REQUIRE(CStringBarName::parameters() == 1);
+
+  REQUIRE(StringFooName::policies() == 0);
+  REQUIRE(CStringFooName::policies() == 0);
+  REQUIRE(StringBarName::policies() == 1);
+  REQUIRE(CStringBarName::policies() == 1);
+
+  REQUIRE(StringFooName::name() == FOO_NAME_STRING);
+  REQUIRE(CStringFooName::name() == FOO_NAME_CSTRING);
+  REQUIRE(StringBarName::name() == BAR_NAME_STRING);
+  REQUIRE(CStringBarName::name() == BAR_NAME_CSTRING);
+}
+
+#if 0
+TEST_CASE("will not compile due to extraneous parameter", "[broken Foo]")
+{
+  using BrokenName = NamePolicyTest<string, int>;
+  REQUIRE(BrokenName::parameters() == 1);
+  REQUIRE(BrokenName::policies() == 0);
+}
+#endif
+
+#if 0
+TEST_CASE("will not compile due to extraneous parameter before policy", "[broken Bar]")
+{
+  using BrokenName = NamePolicyTest<string, int, bar_name_policy>;
+  REQUIRE(BrokenName::parameters() == 2);
+  REQUIRE(BrokenName::policies() == 1);
+}
+#endif
+
+#if 0
+TEST_CASE("will not compile due to extraneous parameter after policy", "[broken Bar]")
+{
+  using BrokenName = NamePolicyTest<string, bar_name_policy, int>;
+  REQUIRE(BrokenName::parameters() == 2);
+  REQUIRE(BrokenName::policies() == 1);
+}
+#endif
