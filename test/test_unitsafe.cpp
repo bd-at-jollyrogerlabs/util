f59f9fe599c20178328da114fde9974db8e3dcd0
Brian Davis <bd@jollyrogerlabs.com>
2016-12-02 08:21:49 -0500

    Added unitsafe support for complex types.
    Some standardization cleanup.

--- test/test_unitsafe.cpp
+++ test/test_unitsafe.cpp
@@ -34,7 +34,9 @@
  *
  */
 
-#include "unitsafe"
+#include "unitsafe_math"
+
+#include <string>
 
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
@@ -42,8 +44,8 @@
 using namespace std;
 using namespace jrl;
 
-struct FooTag {};
-struct BarTag {};
+
+// ******************** simple unitsafe tests ********************
 
 MAKE_UNITSAFE(Foo, unsigned);
 MAKE_UNITSAFE(Bar, unsigned);
@@ -110,9 +112,97 @@ TEST_CASE("Streaming output from reference", "[ostream]")
   REQUIRE(ss.str() == "0");
 }
 
-TEST_CASE("square root", "[sqrt]")
+TEST_CASE("square root, as a proxy for all math functions", "[sqrt]")
 {
+  // NOTE: this test stands in for all math functions as a means of
+  // testing the mechanism in unitsafe_math
   const Foo foo4(4);
   const Foo foo2(sqrt(foo4));
   REQUIRE(2.0 == base_cast(foo2));
+}
+
+
+// ******************** contained unitsafe tests ********************
+
+class DerivedFromEnclosing;
+
+class Enclosing
+{
+  MAKE_PUBLIC_UNITSAFE(Public, int);
+
+  MAKE_CONTAINED_UNITSAFE(Protected, int, protected);
+
+public:
+  Enclosing() = default;
+
+  Enclosing(const int val) : val_(val) {}
+
+protected:
+  friend class DerivedFromEnclosing;
+
+  Protected val_;
+};
+
+class DerivedFromEnclosing : public Enclosing
+{
+public:
+  using Exported = Enclosing::Protected;
+
+  void setVal(const Exported val)
+  {
+    Enclosing::val_ = val;
+  }
+
+  Exported getVal() const
+  {
+    return Enclosing::val_;
+  }
+
+  Exported processEnclosing(const Enclosing &val)
+  {
+    return Enclosing::val_ + val.val_;
+  }
+};
+
+TEST_CASE("basic contained operations", "[contained]")
+{
+  Enclosing encl(1);
+  DerivedFromEnclosing deriv;
+  deriv.setVal(20010910);
+
+  DerivedFromEnclosing mixed;
+  mixed.setVal(deriv.processEnclosing(encl));
+  REQUIRE(20010911 == base_cast(mixed.getVal()));
+}
+
+TEST_CASE("publicly accessible type", "[contained]")
+{
+  using Public = Enclosing::Public;
+  const Public val1(20010910);
+  const Public val2 = 1;
+  const auto result = val1 + val2;
+  REQUIRE(20010911 == base_cast(result));
+}
+
+
+// ******************** complex unitsafe tests ********************
+
+MAKE_COMPLEX_UNITSAFE(Str, std::string);
+
+TEST_CASE("complex unitsafe type for std::string", "[complex]")
+{
+  const Str str1(string("foo"));
+  {
+    ostringstream ss;
+    ss << str1;
+    REQUIRE(ss.str() == "foo");
+  }
+  const Str str2(string("bar"));
+  REQUIRE(str1 != str2);
+  const auto str3 = str1 + str2;
+  {
+    ostringstream ss;
+    ss << str3;
+    REQUIRE(ss.str() == "foobar");
+  }
 }
